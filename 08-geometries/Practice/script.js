944223fcc7596e5f4bee7bb50e97957fafe80608
muhadeyinka <muhadeyinka@gmail.com>
2025-09-17 10:10:08 -0400

    Excercise- Completed Lesson 8, Practice - Used Quaternion and Learned about Instance Mesh

--- 08-geometries/Practice/script.js
+++ 08-geometries/Practice/script.js
@@ -114,7 +114,46 @@ const renderer = new THREE.WebGLRenderer({
     canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.render(scene, camera)
+
+// Event Listeners
+window.addEventListener('resize', () => {
+    // Update Sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update Camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update Renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+window.addEventListener('dblclick', () => {
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+    if(!fullscreenElement){
+        if(canvas.requestFullscreen)
+        {
+            canvas.requestFullscreen()
+        }else if(canvas.webkitRequestFullscreen)
+        {
+            canvas.webkitRequestFullscreen()
+        }
+    }else {
+        if(document.exitFullscreen)
+        {
+            document.exitFullscreen()
+        }else if(document.webkitExitFullscreen)
+        {
+            document.webkitExitFullscreen()
+        }
+    }
+})
+
 
 const planeToCameraQuaternion = new THREE.Quaternion()
 
